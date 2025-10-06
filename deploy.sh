9705d39bad4fcd23222f36dd9f97e21c687c9b30
qyhever <qyhever@126.com>
2025-10-06 11:36:39 +0800

    github-actions

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 dist_path="docs/.vuepress/dist"
-server_name="qyhever"
+server_name="pinco"
 server_static_dic="/usr/local/src/nginx-1.12.2/html"
 scp -r $dist_path/assets $server_name:$server_static_dic
 scp -r $dist_path/pages $server_name:$server_static_dic
